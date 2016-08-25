e2a378f635867b3be777cdc0b5f8eda2ba69899c
Patrick Whalon <pwhalon@eecs.wsu.edu>
2016-08-24 21:40:58 -0700

    Added basic menu with default options

--- linked-list/linked-list/main.c
+++ linked-list/linked-list/main.c
@@ -2,7 +2,41 @@
 
 int main(void)
 {
-	printf("hello world\n");
-	system("pause");
+	int decision = 1;
+	char command = '\0';
+	while (decision != 0)
+	{
+		system("cls");
+		printf("Hi Welcome to Patrick's Digital Music Simulator\n");
+		printf("\tMenu\n");
+		printf("\t1. Add Entry\n");
+		printf("\t2. Delete Entry\n");
+		printf("\t3. Edit Entry\n");
+		printf("\t8. Quit\n");
+
+		command = getchar();
+		fseek(stdin, 0, SEEK_END);
+
+		switch(command)
+		{
+		case '1':
+			printf("adding...");
+			break;
+			//TODO: add function
+		case '2':
+			printf("deleting...");
+			break;
+			//TODO: delete function 
+		case '3':
+			printf("editing...");
+			break;
+			//TODO: edit function
+		case '8':
+			decision = 0;
+			break;
+		default:
+			printf("Error invalid option\n");
+		}
+	}
 	return 0;
 }
